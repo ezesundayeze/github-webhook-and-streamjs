a44ede223cd457a9ddc7e59da16ffe13f84e9d17
Eze Sunday Eze <mailsforeze@gmail.com>
2020-03-05 11:15:32 +0100

    testing somethingb new

--- controllers/index.js
+++ controllers/index.js
@@ -18,7 +18,7 @@ const getToken = (req, res, next)=>{
 const githubHook = async (req, res, next)=>{
     // feedManager(req.body)
     const { user, created_at, state } = req.body.pull_request
-    const feedUser = client.feed('notification', user.login);
+    const feedUser = client.feed('notification', user.login, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkI…plIn0.NK-A9N8-syHcQ8k3evFU9AVikW2mUs9BKha4Lasxa9E");
     await feedUser.addActivity({
         actor: user.login,
         verb: 'add',
